17be0f38c095d579a4707aa2773641f7916374b7
Jeff Giff <90745572+Jeff-Giff@users.noreply.github.com>
2022-04-17 16:09:03 +0800

    Update banner.blade.php

--- resources/views/compenents/banner.blade.php
+++ resources/views/compenents/banner.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <head> 
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-		<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-		<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+	<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+	<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
     <link rel="stylesheet" href="banner">
     {{ $links }}
 </head>
