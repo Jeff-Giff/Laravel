ead89438697d3120ae6ed2a89bdaa63270961a05
Jeff Giff <90745572+Jeff-Giff@users.noreply.github.com>
2022-04-29 15:28:49 +0800

    Add files via upload

--- resources/views/milestones.blade.php
+++ resources/views/milestones.blade.php
@@ -5,13 +5,17 @@
         <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 	    <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
     	<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+        
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">
+        
         <link rel="stylesheet" href="/css/banner.css">
         
     </head>
     <body>
         <div id="banner">
-            <div id="hamburger">=</div><div class="nav">MILESTONES</div><div class="nav">PROJECTS</div><div class="nav">DISCUSSIONS</div><div class="nav">PROBLEMS</div><div class="nav">ADD POST</div><div class="nav">HOME</div>
+            <div id="hamburger">=</div><a class="nav1" href="/milestones">MILESTONES</a><div class="nav1">PROJECTS</div><div class="nav1">DISCUSSIONS</div>            
+            <div class="nav2">PROBLEMS</div><a class="nav2" href="/addpost">ADD POST</a><div class="nav2">HOME</div>
         </div>
-        
     </body>
 </html>
